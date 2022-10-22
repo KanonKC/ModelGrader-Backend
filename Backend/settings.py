095658c3352d2092d7bf4a733c05046dba2e5475
KanonKC <kanonkce@gmail.com>
2022-10-22 22:32:58 +0700

    minor update

--- Backend/settings.py
+++ Backend/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'ModelGrader.sqlite3',
     }
 }
 
