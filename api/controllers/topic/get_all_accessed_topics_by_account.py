6456f5282878425c0eb85f53e2e7dede54698bde
KanonKC <kanonkce@gmail.com>
2024-01-13 17:10:06 +0700

    manual distinct in get accessed topics by account

--- api/controllers/topic/get_all_accessed_topics_by_account.py
+++ api/controllers/topic/get_all_accessed_topics_by_account.py
@@ -11,8 +11,12 @@ from django.db.models import Q
 
 def get_all_accessed_topics_by_account(account:Account):
     groups = [gm.group for gm in GroupMember.objects.filter(account=account)]
-    accessedTopics = TopicGroupPermission.objects.filter(Q(group__in=groups) & (Q(permission_view_topics=True) | Q(permission_manage_topics=True))).distinct()
-    topics = [at.topic for at in accessedTopics]
+    accessedTopics = TopicGroupPermission.objects.filter(Q(group__in=groups) & (Q(permission_view_topics=True) | Q(permission_manage_topics=True)))
+    
+    topics = []
+    for at in accessedTopics:
+        if at.topic not in topics:
+            topics.append(at.topic)
 
     serialize = TopicSerializer(topics,many=True)
 
