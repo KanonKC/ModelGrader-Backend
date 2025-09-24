eb332b4032c612104f38aba534d628535ec4568d
KanonKC <kanonkce@gmail.com>
2025-09-25 00:20:19 +0700

    Return error on wrapper

--- api/wrappers/auth_wrapper.py
+++ api/wrappers/auth_wrapper.py
@@ -1,14 +1,14 @@
-from api.services.auth.auth_service import verify_token
 from api.utility import extract_bearer_token
 from api.errors.common import InvalidTokenError
+from api.setup import auth_service
 
 def authentication_required(function):
     def wrapper(request, *args, **kwargs):
         token = extract_bearer_token(request)
         if not token:
-            raise InvalidTokenError()
-        is_verify = verify_token(token)
+            return InvalidTokenError().django_response()
+        is_verify = auth_service.verify_token(token)
         if not is_verify:
-            raise InvalidTokenError()
+            return InvalidTokenError().django_response()
         return function(request, *args, **kwargs)
     return wrapper
